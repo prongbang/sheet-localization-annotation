42ccfb938a1b4b12eeabd87c85452b59b10a507f
anonymous <anonymous>
2020-10-12 20:17:33 +0700

    add field enabled for disabled/enabled generator

--- library/src/main/java/com/prongbang/aslprocessor/annotation/AndroidSheetLocallization.kt
+++ library/src/main/java/com/prongbang/aslprocessor/annotation/AndroidSheetLocallization.kt
@@ -1,3 +1,7 @@
 package com.prongbang.aslprocessor.annotation
 
-annotation class AndroidSheetLocalization(val documentId: String, val sheetId: String)
+annotation class AndroidSheetLocalization(
+		val documentId: String,
+		val sheetId: String,
+		val enabled: Boolean = true
+)
